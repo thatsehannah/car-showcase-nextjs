f20e4453fbc0852d5231bc57a6dc23756037488c
E. Hannah <echannah631@gmail.com>
2024-04-03 14:26:13 -0500

    added rent per day and other details to car card

--- components/CarCard/CarCard.tsx
+++ components/CarCard/CarCard.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import Image from 'next/image';
 import { CarProps } from '@/types';
 import { CustomButton } from '@/molecules';
+import { calculateCarRent } from '@/utils';
 
 interface CarCardProps {
   car: CarProps;
@@ -11,6 +12,8 @@ interface CarCardProps {
 
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
+  const carRent = calculateCarRent(city_mpg, year);
+
   return (
     <div className='car-card group'>
       <div className='car-card__content'>
@@ -18,9 +21,55 @@ const CarCard = ({ car }: CarCardProps) => {
           {make} {model}
         </h2>
       </div>
-      <p>
-        <span>Car Rent...</span>
+      <p className='flex mt-6 text-[32px] font-extrabold'>
+        <span className='self-start text-[14px] font-semibold'>$</span>
+        {carRent}
+        <span className='self-end text-[14px] font-semibold'>/day</span>
       </p>
+
+      <div className='relative w-full h-40 my-3 object-contain'>
+        <Image
+          src='/hero.png'
+          alt='car model'
+          fill
+          priority
+          className='object-contain'
+        />
+      </div>
+
+      <div className='relative flex w-full mt-2'>
+        <div className='flex group-hover:invisible w-full justify-between text-gray'>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image
+              src={'/steering-wheel.svg'}
+              width={20}
+              height={20}
+              alt='steering wheel'
+            />
+            <p className='text-[14px]'>
+              {transmission === 'a' ? 'Automatic' : 'Manual'}
+            </p>
+          </div>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image
+              src={'/tire.svg'}
+              width={20}
+              height={20}
+              alt='tire'
+            />
+            <p className='text-[14px]'>{drive.toUpperCase()}</p>
+          </div>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image
+              src={'/gas.svg'}
+              width={20}
+              height={20}
+              alt='gas pump'
+            />
+            <p className='text-[14px]'>{city_mpg} MPG</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
