eb65118295fb1604e9a6c85c7ac97f8ef1978fa9
E. Hannah <echannah631@gmail.com>
2024-04-11 07:31:49 -0500

    added model input search bar (no functionality), added search button component

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,10 +1,29 @@
 'use client';
 import { useState } from 'react';
+import Image from 'next/image';
 
 import { SearchManufacturer } from '..';
 
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => {
+  return (
+    <button
+      type='submit'
+      className={`-ml-3 z-10 ${otherClasses}`}
+    >
+      <Image
+        src='/magnifying-glass.svg'
+        alt='magnifying glass'
+        width={40}
+        height={40}
+        className='object-contain'
+      />
+    </button>
+  );
+};
+
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState('');
+  const [model, setModel] = useState('');
   const handleSearch = () => {};
 
   return (
@@ -17,7 +36,27 @@ const SearchBar = () => {
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
+        <SearchButton otherClasses='sm:hidden' />
       </div>
+      <div className='searchbar__item'>
+        <Image
+          src='/model-icon.png'
+          width={25}
+          height={25}
+          className='absolute w-[20px] h-[20px] ml-4'
+          alt='car model'
+        />
+        <input
+          type='text'
+          name='model'
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder='Tiguan'
+          className='searchbar__input'
+        />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <SearchButton otherClasses='max-sm:hidden' />
     </form>
   );
 };
